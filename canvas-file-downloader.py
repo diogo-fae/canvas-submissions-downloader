eaf70d045e219e9ecf13ded051f8aad1668d46b0
diogo-fae <diogo.fae2001@gmail.com>
2023-10-12 23:14:20 -0500

    Gets students names from csv and their submissions
    * Gets student names provided by csv file and gets their IDs
    * If no students are provided, gets all IDs
    * Updated gitignore

--- canvas-file-downloader.py
+++ canvas-file-downloader.py
@@ -1,5 +1,7 @@
-from canvasapi import Canvas, requester
+import csv
 import json
+
+from canvasapi import Canvas
 
 # Canvas API URL
 API_URL = "https://smu.instructure.com/"
@@ -15,5 +17,24 @@ canvas = Canvas(API_URL, TOKEN)
 course = canvas.get_course(course_ID)
 assignment = course.get_assignment(assignment_ID)
 
-for s in assignment.get_submissions():
-    print(s)
+# Get IDs for students
+# Get students into dictionary so it is faster to check if student is in dictionary
+students_to_grade = {}
+
+# Get students given in csv file
+with open("students.csv", "r") as file:
+    csv_reader = csv.reader(file)
+    for row in csv_reader:
+        for name in row:
+            students_to_grade[name] = None
+
+# If there are no students provided, get IDs for them all
+all_students = course.get_users(enrollment_type=["student"])
+if students_to_grade:
+    students_to_grade = {
+        student.name: student.id
+        for student in all_students
+        if student.name in students_to_grade
+    }
+else:
+    students_to_grade = {student.name: student.id for student in all_students}
